95d6c2b90eb105564f30f20d8f056ca52b620e98
Sarah Ewing <sarah.ewing@shyftsolutions.io>
2025-01-21 18:17:07 -0600

    adding boat_slip table and populating it

--- moffat_bay_db_init.sql
+++ moffat_bay_db_init.sql
@@ -6,13 +6,13 @@
 */
 
 -- drop database moffat-bay if exists
-DROP DATABASE moffat-bay;
+DROP DATABASE `moffat-bay`;
 
 -- create database moffat-bay
-CREATE DATABASE moffat-bay;
+CREATE DATABASE `moffat-bay`;
 
 -- use the database
-USE moffat-bay;
+USE `moffat-bay`;
 
 -- drop database user if exists
 DROP USER IF EXISTS 'dbadmin'@'localhost';
@@ -21,7 +21,7 @@ DROP USER IF EXISTS 'dbadmin'@'localhost';
 CREATE USER 'dbadmin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'master01';
 
 -- grant all privileges to the moffat-bay database to user dbadmin on localhost
-GRANT ALL PRIVILEGES ON moffat-bay.* TO 'dbadmin'@'localhost';
+GRANT ALL PRIVILEGES ON `moffat-bay`.* TO 'dbadmin'@'localhost';
 
 -- drop tables if they are present
 DROP TABLE IF EXISTS customer;
@@ -35,6 +35,11 @@ CREATE TABLE customer (
 
 -- create the boat_slip table
 CREATE TABLE boat_slip (
+    slip_id     INT         NOT NULL    AUTO_INCREMENT,
+    in_use      BOOLEAN     NOT NULL,
+    slip_size   INT         NOT NULL    CHECK (slip_size IN (26, 40, 50)),
+
+    PRIMARY KEY(slip_id)
 );
 
 -- create the reservation table
@@ -52,8 +57,38 @@ VALUES
 ;
 
 -- insert boat_slip records
-INSERT INTO boat_slip ()
+INSERT INTO boat_slip (in_use, slip_size)
 VALUES
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 26),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 40),
+(FALSE, 50),
+(FALSE, 50),
+(FALSE, 50),
+(FALSE, 50),
+(FALSE, 50)
 ;
 
 -- insert reservation records
