ec0b2f4766c9f9dfb56a76ecc49c414f3c439f28
erosales48 <erosales48@pm.me>
2025-01-31 15:18:54 -0500

    fixing it to work again

---
+++ src/main/java/com/moffatbay/utils/ConfigLoader.java
@@ -0,0 +1,23 @@
+package com.moffatbay.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("local-config.properties")) {
+            if (input != null) {
+                properties.load(input);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
