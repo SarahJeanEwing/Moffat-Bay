cd2599aad99b1a08e5294437b2993401482985e4
Sarah Ewing <sarah.ewing@shyftsolutions.io>
2025-02-08 07:38:01 -0600

    making minor changes to improve flow

--- src/main/java/com/moffatbay/servlets/ReservationInfoServlet.java
+++ src/main/java/com/moffatbay/servlets/ReservationInfoServlet.java
@@ -1,27 +1,20 @@
 package com.moffatbay.servlets;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 import java.io.Serial;
 
 
-@WebServlet("/RedirectServlet")
-public class RedirectServlet extends HttpServlet {
+@WebServlet("/reservations-info")
+public class ReservationInfoServlet extends HttpServlet {
+    @Serial
     private static final long serialVersionUID = 1L;
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        boolean isLoggedIn = (session != null && session.getAttribute("user") != null);
-
-        if (isLoggedIn) {
-            response.sendRedirect("/* add correct jsp.jsp");*/
-        } else {
-            response.sendRedirect("login.jsp");
-        }
+        request.getRequestDispatcher("/reservations-info.jsp").forward(request, response);
     }
 }
--- src/main/java/com/moffatbay/servlets/ReservationUserConfirmsServlet.java
+++ src/main/java/com/moffatbay/servlets/ReservationUserConfirmsServlet.java
@@ -1,13 +1,15 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+package com.moffatbay.servlets;
 
-@WebServlet("/ReservationServlet")
-public class ReservationServlet extends HttpServlet {
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/Reservation-Confirmation")
+public class ReservationUserConfirmsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
